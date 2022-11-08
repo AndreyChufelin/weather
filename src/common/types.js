a893f78a90e31fac937f5f990dd9d703ef81f416
Andrey Chufelin <chufelin.andrey@gmail.com>
2022-11-08 17:20:37 +0300

    update dist

--- src/common/types.js
+++
@@ -1,9 +0,0 @@
-export const languages = {
-  ru: "ru",
-  eng: "en",
-};
-export const statuses = {
-  loading: "loading",
-  ready: "ready",
-  error: "error",
-};
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import "./assets/styles/style.css";
-import "swiper/swiper.min.css";
-import { i18n } from "./common/localization";
-
-createApp(App).use(store).use(router).use(i18n).mount("#app");
