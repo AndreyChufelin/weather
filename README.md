f22400aa8840b4a895ab395ff902707006fb91ac
Andrey Chufelin <chufelin.andrey@gmail.com>
2022-11-08 16:49:29 +0300

    update README.md and .env

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # doggoweather
+[demo](https://andreychufelin.github.io/weather/)
+
+Displays weather data
+
+![alt text](https://github.com/AndreyChufelin/weather/raw/master/screenshots/main.png?raw=true)
 
 ## Project setup
 ```
 npm install
+```
+
+## Set .env
+```
+VUE_APP_VUE_WEATHER_API_KEY - https://openweathermap.org/
+VUE_APP_VUE_GEO_API_KEY - https://rapidapi.com/wirefreethought/api/geodb-cities/
 ```
 
 ### Compiles and hot-reloads for development
--- src/api/location.js
+++ src/api/location.js
@@ -3,10 +3,12 @@ import { i18n } from "../common/localization";
 
 // const { t } = useI18n({});
 const { t, locale } = i18n.global;
+console.log("f", process.env, process.env.VUE_APP_WEATHER_API_KEY);
 
 const domain = "https://wft-geo-db.p.rapidapi.com";
 const headers = {
-  "x-rapidapi-key": "3ec7de8ea2msh6b4957c9a6d946fp1870e4jsnf5d4bab37dd5",
+  "X-RapidAPI-Key": process.env.VUE_APP_GEO_API_KEY,
+  "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
 };
 function toCoords(x) {
   let parsed = x.toFixed(4);
@@ -46,21 +48,22 @@ export const getCityByCoordsApi = (coords) => {
   const longitude = toCoords(coords.longitude);
   const parsedCoords = latitude + longitude;
 
-  return call(`/v1/geo/locations/${parsedCoords}/nearbyCities`).then((res) => {
+  return call(`v1/geo/locations/${parsedCoords}/nearbyCities`).then((res) => {
     return res.data[0];
   });
 };
 
 export const getCityByIdApi = (id) => {
-  return call(`/v1/geo/cities/${id}`, {}).then((res) => {
+  return call(`v1/geo/cities/${id}`, {}).then((res) => {
     return res.data;
   });
 };
 
 export const searchCitiesApi = (namePrefix) => {
-  return call(`/v1/geo/cities`, { namePrefix, sort: "-population" }).then(
-    (res) => {
-      return res.data;
-    }
-  );
+  return call(`v1/geo/cities`, {
+    namePrefix,
+    sort: "-population",
+  }).then((res) => {
+    return res.data;
+  });
 };
